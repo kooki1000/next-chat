d10c74caa5b9979f5f5b29734c149012602f7b40
Koki Ukai <kooki.ukai100@icloud.com>
2025-07-09 14:06:56 +0900

    feat: add custom loader component on initial render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { Loader } from "lucide-react";
 import dynamic from "next/dynamic";
 
 const FrontendApp = dynamic(() => import("@/frontend"), {
   ssr: false,
-  loading: () => <div>Loading...</div>,
+  loading: LoadingComponent,
 });
 
 export default function App() {
   return <FrontendApp />;
+}
+
+function LoadingComponent() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center">
+      <Loader className="w-12 h-12 animate-spin text-foreground/80" />
+    </main>
+  );
 }
