dd5e4e4b42c584617428f638d239bf85a597aea7
Koki Ukai <kooki.ukai100@icloud.com>
2025-07-09 11:47:31 +0900

    chore: update eslint config with '@antfu/eslint-config' plugin

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import antfu from "@antfu/eslint-config";
+import nextPlugin from "@next/eslint-plugin-next";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+export default antfu({
+  react: true,
+  typescript: true,
+  stylistic: {
+    indent: 2,
+    quotes: "double",
+    semi: true,
+  },
+  formatters: true,
+  ignores: ["next-env.d.ts"],
+}, jsxA11y.flatConfigs.recommended, {
+  plugins: {
+    "@next/next": nextPlugin,
+  },
+  rules: {
+    ...nextPlugin.configs.recommended.rules,
+    ...nextPlugin.configs["core-web-vitals"].rules,
+  },
+}, {
+  files: ["**/*.{jsx,tsx}"],
+  languageOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  },
+  plugins: {
+    "better-tailwindcss": eslintPluginBetterTailwindcss,
+  },
+  settings: {
+    "better-tailwindcss": {
+      entryPoint: "src/app/globals.css",
+    },
+  },
+  rules: {
+    ...eslintPluginBetterTailwindcss.configs["recommended-warn"].rules,
+    "better-tailwindcss/enforce-consistent-line-wrapping": "off",
+  },
+}, {
+  rules: {
+    "antfu/no-top-level-await": "off",
+    "node/prefer-global/process": "off",
+  },
+});
